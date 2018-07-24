bba1ccf2f1a06a286b6eb7985ff60ac44683dd80
Kristian Padron <kpadron.github@gmail.com>
2018-07-24 17:22:36 -0400

    added preliminary file tree parsing

--- scanner.c
+++ scanner.c
@@ -7,8 +7,10 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-#include "scanner.h"
+// #include "scanner.h"
 #include "hash.h"
 
 // SCAN FILE OR DIRECTORY AND SAVE METADATA
@@ -20,57 +22,73 @@ double wtime(void);
 uint32_t rand32(void);
 uint64_t rand64(void);
 
+void file_recursive(char* filepath);
+char* stracat(char* left, char* right);
+
 int main(int argc, char** argv)
 {
-    char* tests[] = { "hash_insert", "hash_search", "hash_remove" };
+    char* filepath = NULL;
+
     if (argc < 2)
     {
         printf("Usage: scanner <filepath> ...\n");
     }
 
-    hash_t table;
-
-    hash_init(&table, 10);
-
-
-    for (size_t i = 0; i < 3; i++)
+    if (argc > 1)
     {
-        double test_duration = 5;
-        double test_start = 0;
-        double test_time = 0;
-        size_t test_cycles = 0;
-
-        do
-        {
-            uint32_t k = rand32();
-
-            test_start = wtime();
-            if (!strcmp(tests[i], "hash_insert"))
-            {
-                hash_insert(&table, k, NULL);
-            }
-            else if (!strcmp(tests[i], "hash_search"))
-            {
-                hash_search(&table, k);
-            }
-            else if (!strcmp(tests[i], "hash_remove"))
-            {
-                hash_remove(&table, k);
-            }
-            test_time += wtime() - test_start;
-
-            test_cycles++;
-
-        } while (test_time < test_duration);
-
-        printf("%s: %zu iterations over %.2f s -> %.4f ns per operation\n", tests[i], test_cycles, test_time, test_time * 1E9 / test_cycles);
+        filepath = strdup(argv[1]);
     }
 
-    hash_free(&table);
+    file_recursive(filepath);
 
     return 0;
 }
 
+
+void file_recursive(char* filepath)
+{
+    struct stat filestat;
+
+    if (stat(filepath, &filestat) < 0)
+    {
+        perror("stat");
+        return;
+    }
+
+    if (S_ISREG(filestat.st_mode))
+    {
+        printf("%s: %zu\n", filepath, filestat.st_size);
+    }
+    else if (S_ISDIR(filestat.st_mode))
+    {
+        DIR* dir = opendir(filepath);
+
+        struct dirent* dp;
+        while ((dp = readdir(dir)) != NULL)
+        {
+            if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..")) continue;
+
+            char* newpath = strdup(filepath);
+
+            if (newpath[strlen(newpath) - 1] != '/')
+                newpath = stracat(newpath, "/");
+
+            newpath = stracat(newpath, dp->d_name);
+
+            file_recursive(newpath);
+            free(newpath);
+        }
+
+        closedir(dir);
+    }
+}
+
+
+char* stracat(char* left, char* right)
+{
+    left = (char*) realloc(left, strlen(left) + strlen(right) + 1);
+    return strcat(left, right);
+}
 
 // walltime of the computer in seconds (useful for performance analysis)
 double wtime(void)
